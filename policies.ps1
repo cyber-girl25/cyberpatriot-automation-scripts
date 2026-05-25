958851d2f338e8c9e9fc28f976973fac79766bfa
Aanya Singh <singh.aanya@hotmail.com>
2026-05-25 15:49:30 -0700

    Added functions to read and write policies managed by Secedit.exe, finished the code for policy 'Everyone may not access this computer from the network'

--- policies.ps1
+++ policies.ps1
@@ -20,40 +20,37 @@ function Prompt-Policy {
     return $result
 }
 
-#########################################################################################
-
-$currentPolicy = "A sufficient password history is being kept"
-$checkEnforcePasswordHistory = Prompt-Policy -policyName $currentPolicy
-
-if ($checkEnforcePasswordHistory -eq 0) {
-    # Informing the user
-    Write-Host "`n`t[!] Checking '$currentPolicy'"
-
+function Get-SeceditPolicy {
+    param(
+        $area,
+        $policyName
+    )
     # Creating the temporary file for security stuff
     $tempFile = "$env:TEMP\secpolicy.inf"
-    secedit /export /cfg $tempFile /areas SECURITYPOLICY | Out-Null
+    secedit /export /cfg $tempFile /areas $area | Out-Null
 
     # Find the policy line
-    $policyLine = Get-Content $tempFile | Select-String "PasswordHistorySize"
-
-    # Get the value from the line
-    $value = [int](($policyLine -split "=")[1].Trim())
+    $policyLine = Get-Content $tempFile | Select-String $policyName
 
     # Get rid of the temp file
     Remove-Item $tempFile
 
-    # If the value is 5 or greater, tell that to the user. Otherwise, tell the user the value and ask if they want it changed to 5.
-    if ($value -ge 5) {
-        Write-Host "`n`t`t$value passwords are remembered."
-    } else {
-        Write-Host "`n`t`t$value password(s) are remembered. Changing this to 5..."
+    # Return the policy
+    return $policyLine
+}
 
-        # Create the configuration string
+function Write-SeceditPolicy {
+    param(
+        $area,
+        $policyHeader,
+        $policyLine
+    )
+    # Create the configuration string
         $config = @"
 [Unicode]
 Unicode=yes
-[System Access]
-PasswordHistorySize = 5
+[$policyHeader]
+$policyLine
 [Version]
 signature="`$CHICAGO`$"
 Revision=1        
@@ -64,13 +61,38 @@ Revision=1
         $config | Out-File -Filepath $importFile -Encoding ascii
 
         # import this into the Windows Security Database
-        secedit /configure /db $env:windir\security\local.sdb /cfg $importFile /areas SECURITYPOLICY | Out-Null
+        secedit /configure /db $env:windir\security\local.sdb /cfg $importFile /areas $area | Out-Null
 
         # Remove the temp file as it is not needed anymore
         Remove-Item $importFile
 
         # Force systemwide updates
         gpupdate /force | Out-Null
+}
+
+#########################################################################################
+
+$currentPolicy = "A sufficient password history is being kept"
+$checkEnforcePasswordHistory = Prompt-Policy -policyName $currentPolicy
+
+if ($checkEnforcePasswordHistory -eq 0) {
+    # Informing the user
+    Write-Host "`n`t[!] Checking '$currentPolicy'"
+
+    # Get policy line
+    $policyLine = Get-SeceditPolicy -area SECURITYPOLICY -policyName PasswordHistorySize
+
+    # Get the value from the line
+    $value = [int](($policyLine -split "=")[1].Trim())
+
+    # If the value is 5 or greater, tell that to the user. Otherwise, tell the user the value and ask if they want it changed to 5.
+    if ($value -ge 5) {
+        Write-Host "`n`t`t$value passwords are remembered."
+    } else {
+        Write-Host "`n`t`t$value password(s) are remembered. Changing this to 5..."
+
+        # Write the policy
+        Write-SeceditPolicy -area "SECURITYPOLICY" -policyHeader "System Access" -policyLine "PasswordHistorySize = 5"
 
         Write-Host "`n`n`t`t[!] Changed policy to 5 passwords remembered."
     }
@@ -78,11 +100,32 @@ Revision=1
     Write-Host "`n`t[!] Skipping '$currentPolicy'"
 }
 
+#########################################################################################
+
 $currentPolicy = "Everyone may not access this computer from the network"
 $checkAccessThisComputerFromTheNetwork = Prompt-Policy -policyName $currentPolicy
 
 if ($checkAccessThisComputerFromTheNetwork -eq 0) {
     Write-Host "`n`t[!] Checking '$currentPolicy'"
+
+    # Get the policy line
+    $policyLine = Get-SeceditPolicy -area "USER_RIGHTS" -policyName "SeNetworkLogonRight"
+
+    # Get the values
+    $values = (($policyLine -split "=")[1].Trim() -split ",")
+    # If everyone is in the values, remove it.
+    if ("*S-1-1-0" -in $values) {
+        Write-Host "`n`t`tThe group Everyone can access this computer from the network. Removing..."
+
+        # Determine the new value
+        $newValues = ($values -ne "*S-1-1-0") -join ","
+        $newPolicy = "SeNetworkLogonRight = $newValues"
+        
+        # Write the policy
+        Write-SeceditPolicy -area "USER_RIGHTS" -policyHeader "Privilege Rights" -policyLine $newPolicy
+
+        Write-Host "`n`n`t`t[!] Removed Everyone from policy."
+    }
 } else {
     Write-Host "`n`t[!] Skipping '$currentPolicy'"
 }
