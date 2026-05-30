e338bea680be83aef227cfc1b494c5e7a25b8a35
Aanya Singh <singh.aanya@hotmail.com>
2026-05-29 22:08:27 -0700

    Update README and add new policy to users.ps1 Add policy: Remove any users that should not be administrators from administrators
    Document in README
    
    Other changes to user.ps1:
     - Fix some indentation
     - Remove unnecessary output
     - Add comments to clarify some things
     - Add forced password reset to new users
     - Add some necessary output

--- users.ps1
+++ users.ps1
@@ -50,9 +50,9 @@ if ($checkUnauthorizedUsers -eq 0) {
     # Get list of authorized users
     $prompt = @"
 
-    Enter the authorized users list. Separate with commas.
-    Example: alice,bob,charlie,jane
-    Users
+Enter the authorized users list. Separate with commas.
+Example: alice,bob,charlie,jane
+Users
 "@
 
     $authorizedUsers = @((Read-Host $prompt) -split ",")
@@ -72,8 +72,6 @@ if ($checkUnauthorizedUsers -eq 0) {
             if ($authorizedUsers -notcontains $currentUser) {
                 $deleteUser = Prompt-User -userName $currentUser -action "User '$currentUser' is not in the provided list of authorized users. Would you like to delete them?"
                 if ($deleteUser -eq 0) {
-                    Write-Host "`n`t`tRemoving user '$currentUser'..."
-
                     # Remove user
                     Remove-LocalUser -Name $currentUser
 
@@ -84,7 +82,6 @@ if ($checkUnauthorizedUsers -eq 0) {
             }
         }
     }
-    Write-Host "`n`t`t`t[!] Checked all users"
 } else {
     Write-Host "`n`t[!] Skipping '$currentPolicy'"
 }
@@ -99,30 +96,81 @@ if ($doAddUsers -eq 0) {
     # Get list of users to add
     $prompt = @"
 
-    Enter the list of users to make accounts for. Separate with commas.
-    Example: carlisle,burma,emma
-    New users
+Enter the list of users to make accounts for. Separate with commas.
+Example: carlisle,burma,emma
+New users
 "@
     $usersToAdd = @((Read-Host $prompt) -split ",")
 
     # Get the secure password
     $prompt = @"
 
-    Enter the secure password. If none is provided, the default 'CyberPatriot123!' will be used.
-    Password
+Enter the secure password. If none is provided, the default 'CyberPatriot123!' will be used.
+Password
 "@
     $password = Read-Host $prompt -AsSecureString
 
+    # If none is provided, set password to secure default
     if ($password -eq "") {
         $passwordText = "CyberPatriot123!"
         $password = ConvertTo-SecureString $passwordText -AsPlainText -Force
     }
 
+    # Make accounts with passwords
     foreach ($user in $usersToAdd) {
+            # Create account
             New-LocalUser -Name $user -Password $password | Out-Null
+
+            # Force password reset on next login
+            net user $user /logonpasswordch:yes | Out-Null
+
             Write-Host "`n`t`t[!] Added user '$user'"
         }
-    Write-Host "`n`t`t[!] Added all users"
+} else {
+    Write-Host "`n`t[!] Skipping '$currentPolicy'"
+}
+
+#########################################################################################
+
+$currentPolicy = "Remove any users that should not be administrators from administrators"
+$checkAdmins = Prompt-Policy -policyName $currentPolicy
+
+if ($checkAdmins -eq 0) {
+    Write-Host "`n`t[!] Checking '$currentPolicy'"
+
+    # Get admin list
+    $prompt = @"
+Enter the authorized administrators list. Separate with commas.
+Example: ellise,jamielou,sydney,caitlin,rachel
+Users
+"@
+    $adminNames = @((Read-Host $prompt) -split ",")
+
+    $admins = @()
+    foreach ($adminName in $adminNames) {
+        $admins += "$env:COMPUTERNAME\$adminName"
+    }
+    
+    # Get current admins
+    $currentAdmins = @((Get-LocalGroupMember -Group "Administrators").Name)
+
+    # Loop through current admins, see if it isn't Administrator, if it isn't in admins. If so, ask to remove.
+    foreach ($currentAdmin in $currentAdmins) {
+        if ($currentAdmin -ne "CP\Administrator" -and $admins -notcontains $currentAdmin) {
+            $removeAdmin = Prompt-User -userName $currentAdmin -action "User '$currentAdmin' is not in the provided list of authorized admins. Would you like to remove them?"
+                if ($removeAdmin -eq 0) {
+                    Write-Host "`n`t`tRemoving admin '$currentAdmin'..."
+
+                    # Remove user
+                    Remove-LocalGroupMember -Group "Administrators" -Member $currentAdmin
+
+                    Write-Host "`n`t`t[!] Removed admin '$currentAdmin'"
+                } else {
+                    Write-Host "`n`t`tIgnoring user '$currentAdmin'..."
+                }
+        }
+    }
+    Write-Host "`n`t`t[!] Removed all unauthorized admins"
 } else {
     Write-Host "`n`t[!] Skipping '$currentPolicy'"
 }
